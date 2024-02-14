bfd67d1b4911f628ca06978db3f60364c1459e77
GDIATTA <147615966+GDIATTA@users.noreply.github.com>
2024-02-14 15:53:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
-if __name__=="__main__":
-    app.run(debug=True)
+## if __name__=="__main__":
+##   app.run(debug=True)
